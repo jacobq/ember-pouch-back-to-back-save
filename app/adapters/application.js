870a8e7c1ef8a1f86ba13fdb668c953fc31ae041
Jacob Quant <jacobq@gmail.com>
2018-11-13 16:21:13 -0600

    Enable PouchDB debugging

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,12 @@
 import PouchDB from 'pouchdb';
+import PouchDebugPlugin from 'pouchdb-debug';
 import { Adapter } from 'ember-pouch';
 
 export default Adapter.extend({
   init() {
     this._super(...arguments);
+    PouchDB.plugin(PouchDebugPlugin);
+    PouchDB.debug.enable('*');
     this.set('db', new PouchDB('_example_'));
   }
 });
